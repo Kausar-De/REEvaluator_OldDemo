eac449512779dad2c965c36cdb82c328393599cb
Kausar De <kde48business@gmail.com>
2023-04-11 11:49:02 +0530

    Update settings.py
    Obscuring Django Security Key

--- refficalc/settings.py
+++ refficalc/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q+*qrmqm4(-101c@owm$tg8s#r^nmv$hzfv4*(pc5)3)ikaql-'
+SECRET_KEY = 'OBSCURED'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
